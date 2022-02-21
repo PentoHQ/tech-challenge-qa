a88636f65d1f693274a0ba79cd32394988570d8a
lee mcgeever <leemcgeever@gmail.com>
2022-02-21 09:22:49 +0000

    Update verifyEndpoints.spec.js
    Updated the test decriptions for clarity

--- cypress/integration/api/verifyEndpoints.spec.js
+++ cypress/integration/api/verifyEndpoints.spec.js
@@ -2,15 +2,15 @@
 
 // Tests to be run on a separate environemnt to the API Tests initially TO-DO:  Clear data between runs
 describe('Verify the GET API Endpoint', () => {
-    it('Test GET Request: Verify the body is empty as no data created.  No Seeded data ', () => {
+    it('Test GET Request: Verify the response returns correct status code and in a timely manner', () => {
         cy.request('http://localhost:3001/api/sessions')
                  .then((response) => {
                     expect(response.status).to.eq(200);
                     expect(response.duration).to.be.lessThan(200)
             })
     });
-
-    it('Test POST Request', () => {
+    
+    it('Test POST Request creates a new record and returns correct status code and in a timely manner ', () => {
         cy.createUserViaApi()
     });
 })
